9307df95e51cfeddcadcb7b223b2119072e49959
faza_elhafs <najah.hafshoh@gmail.com>
2023-12-29 09:47:07 +0700

    - install prisma - connect prisma

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// LOAD ENV
+import dotenv from 'dotenv'
+dotenv.config();
+
 import express from "express";
 import cookieParser from "cookie-parser";
 
@@ -43,6 +47,8 @@ app.use(routerAuth);
 // MIDDLEWARE UNTUK PATH ASING / UNKNOWN PAGE
 app.use(notFound);
 
-app.listen(5000, () => {
-    console.info("App is running in http://localhost:5000")
+const port = process.env.PORT || 5000;
+console.log(port);
+app.listen(port, () => {
+    console.info("App is running in http://localhost:" + port)
 });
