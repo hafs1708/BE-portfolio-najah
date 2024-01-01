11310a6955b7b0c3b4ebc10e54e7eef012844923
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-01 09:29:58 +0700

    Changes router blog

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import blogController from '../controller/blogController.js';
 
-
 export const routerBlog = express.Router();
 
+// get all blogs
 routerBlog.get('/blogs', blogController.getAll);
-routerBlog.get('/blog/:id', blogController.get);
+// save new blog
 routerBlog.post('/blog', blogController.post);
+// update title only
+routerBlog.patch('/update_blog_title/:id', blogController.updateTitle);
 
 routerBlog.route('/blog/:id')
-    .put(blogController.put)
-    .delete(blogController.remove);
-
-routerBlog.patch('/update_blog_title/:id', blogController.updateTitle);
+    .get(blogController.get) // get by id
+    .put(blogController.put) // update by id
+    .delete(blogController.remove); // remove by id
