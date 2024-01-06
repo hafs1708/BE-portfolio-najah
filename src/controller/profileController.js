b7b5b4f3c8319cd5dee6362f6a975a4ed39e0d05
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-06 08:58:07 +0700

    create dummy

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -1,17 +1,32 @@
-//  PATH: METHOD GET UNTUK MENGAMBIL DATA PROFILE
-const get = (req, res) => {
-    // ambil data dari database
-    const data = {
-        firstname: "Faiqis",
-        lastname: "Rahmany",
-        email: "faiqisrahmany@gmail.com",
-        age: 19
-    };
+import { Prisma } from "../application/prisma.js";
 
-    res.status(200).json({
-        message: "Berhasil",
-        data: data
-    });
+//  PATH: METHOD GET UNTUK MENGAMBIL DATA PROFILE
+const get = async (req, res, next) => {
+    try {
+        // CEK KE DATABASE
+        let profile = await Prisma.profile.findFirst();
+
+        // JIKA KOSONG => KIRIM DATA DUMMY
+        if (!profile) {
+            // buat data dummy
+            profile = {
+                email: "example@gmail.com",
+                firstName: "-",
+                lastName: "-",
+                dob: "1900-01-01",
+                address: "-"
+            };
+        }
+
+        // KALO ADA ISINYA => KIRIM DATA ASLI
+        res.status(200).json({
+            message: "Berhasil ambil data profile",
+            data: profile
+        });
+    } catch (error) {
+        next(error);
+    }
+
 };
 
 // PATH: METHOD PUT UNTUK MENYIMPAN SELURUH DATA PROFILE
--- src/router/publicRouter.js
+++ src/router/publicRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authController from '../controller/authController.js';
 import blogController from '../controller/blogController.js';
+import profileController from '../controller/profileController.js';
+import educationController from '../controller/educationController.js';
 
 export const routerPublic = express.Router();
 
@@ -13,7 +15,7 @@ routerPublic.get('/blog/:id', blogController.get) // get by id
 
 // EDUCATION
 routerPublic.get('/educations', educationController.getAll);
-routerPublic.route('/education/:id', educationController.get);
+routerPublic.get('/education/:id', educationController.get);
 
 // PROFILE
 routerPublic.get('/profile', profileController.get) // get profile
