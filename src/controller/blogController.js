bcbaec40aae4957721e27bc88312b91ecfe4694a
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-25 09:43:13 +0700

    fix blog & project controller

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -30,7 +30,7 @@ const getAll = async (req, res, next) => {
     }
 };
 
-const getByPage = async (page, limit = 10) => {
+const getByPage = async (page = 1, limit = 10) => {
     // SKIP
     const skip = (page - 1) * limit;
 
