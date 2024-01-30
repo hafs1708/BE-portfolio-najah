18da2523cbf50ea56fe479fb7b9271bd419e13ba
faza_elhafs <najah.hafshoh@gmail.com>
2024-01-30 11:34:46 +0700

    handle multer

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,35 +1,17 @@
 import express from 'express';
 import blogController from '../controller/blogController.js';
-import multer from 'multer';
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-        // date + random number
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-
-        // get file extention
-        const ext = file.originalname.split('.').pop();
-
-        // cb(null, `${file.fieldname}-${uniqueSuffix}.${ext}`);
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + ext);
-    }
-})
-
-const upload = multer({ storage: storage });
+import fileService from '../service/fileService.js';
 
 export const routerBlog = express.Router();
 
 // save new blog
-routerBlog.post('/blog', upload.array('photos', 10), blogController.post);
+routerBlog.post('/blog', fileService.upload.array('photos', 10), blogController.post);
 
 // update title only
 routerBlog.patch('/update_blog_title/:id', blogController.updateTitle);
 
 // update blog + photo
-routerBlog.put('/blog/:id', upload.array('photos', 10), blogController.put) // update by id
+routerBlog.put('/blog/:id', fileService.upload.array('photos', 10), blogController.put) // update by id
 
 // delete
 routerBlog.delete('/blog/:id', blogController.remove); // remove by id
--- src/router/profile.js
+++ src/router/profile.js
@@ -1,26 +1,7 @@
 import express from 'express';
 import profileController from '../controller/profileController.js';
-import multer from 'multer';
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-        // date + random number
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-
-        // get file extention
-        const ext = file.originalname.split('.').pop();
-
-        // cb(null, `${file.fieldname}-${uniqueSuffix}.${ext}`);
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + ext);
-    }
-})
-
-const upload = multer({ storage: storage })
+import fileService from '../service/fileService.js';
 
 export const routerProfile = express.Router();
 
-// update profilehttps://discord.com/channels/1105443314880233484/1105443315446468684
-routerProfile.put('/profile', upload.single('avatar'), profileController.put);
+routerProfile.put('/profile', fileService.upload.single('avatar'), profileController.put);
